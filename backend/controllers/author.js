58f9dff582561c46f0d8338546e5d5e4b05c4c04
souhail <souhailbenslimene@outlook.com>
2022-01-20 16:00:24 +0100

    fix update authors

--- backend/controllers/author.js
+++ backend/controllers/author.js
@@ -17,17 +17,15 @@ exports.read = async (req, res) => {
     res.status(200).json(await Author.find());
 };
 exports.update = (req, res) => {
-    Author.findById(req.params.id, async (err, author) => {
-        if (err) {
-            res.status(500).send({message: err});
-            return;
-        }
-        if (req.body.name) {
-            author.name = req.body.name;
-        }
-        await author.save();
-        res.status(200).json(author);
-    });
+    if (req.body.name) {
+        Author.findByIdAndUpdate(req.params.id, {name: req.body.name}, async (err, author) => {
+            if (err) {
+                res.status(500).send({message: err});
+                return;
+            }
+            res.status(200).json(await Author.findById(req.params.id));
+        });
+    }
 };
 exports.delete = (req, res) => {
     Author.findById(req.params.id, async (err, author) => {
